d3ff78dae8e7ae0555823b570652aaeae93cf1ca
billonas <vas1l1skaz1s@gmail.com>
2012-04-20 14:23:10 +0300

    Πρωταρχική δουλεία για Model User

--- app/Model/User.php
+++ app/Model/User.php
@@ -5,9 +5,46 @@
  *
  * @author Chris Keklikopoulos,Billonas
  */
-class User extends AppModel{
-    var $name= 'User';
-    //put your code here
+class User extends AppModel
+{
+      var $name= 'User';
+
+      public $validate = array(  
+      'username'=>array(  
+            'rule'=>'notEmpty',
+            'required'=>true,
+            'allowEmpty'=>false,  
+            'message'=>'Παρακαλούμε πληκτρολογήστε το κωδικό όνομα'  
+      ),  
+      'password'=>array(  
+            'rule'=>'notEmpty',  
+            'required'=>true,  
+            'allowEmpty'=>false,  
+            'message'=>'Παρακαλούμε πληκτρολογήστε το κωδικό σας'  
+      )  
+      ); 
+
+      function validate_user($data)
+      {
+         $return = false;
+         
+         $conditions = array(
+             'User.username'=>$data['User']['username'],
+          );
+         //βρες αν υπάρχει ο χρήστης με το συγκεκριμένο username
+         $user = $this->find('first', array('conditions'=>$conditions));
+
+         //εάν υπάρχει
+         if(!empty($user))
+         {
+            //έλενξε τον κωδικό
+            if($user['User']['password'] == ($data['User']['password']))
+            {
+               $return=$user;
+            }
+         }
+         return $return;
+      }
 }
 
 ?>
