2023df7734a025c45346d3cb72e2f6683adb0cc9
Ilias Chalkidis <sdi0700122@di.uoa.gr>
2012-04-29 14:10:48 +0300

    [Fix database.php Config file]

--- app/Config/database.php
+++ app/Config/database.php
@@ -62,22 +62,13 @@ class DATABASE_CONFIG {
 	public $default = array(
 		'datasource' => 'Database/Mysql',
 		'persistent' => false,
-		'host' => 'localhost',
-		'login' => 'root',
-		'password' => 'root',
+		'host' => 'devnet.su',
+		'login' => 'elkethedev',
+		'password' => 'd3v5@3lk3th3pr0j3ct',
 		'database' => 'elkethe',
 		'prefix' => '',
 		//'encoding' => 'utf8',
 	);
-        
-//        'datasource' => 'Database/Mysql',
-//        'persistent' => false,
-//        'host' => 'devnet.su',
-//        'login' => 'elkethedev',
-//        'password' => 'd3v5@3lk3th3pr0j3ct',
-//        'database' => 'elkethe',
-//        'prefix' => '',
-//        //'encoding' => 'utf8',
 
 	public $test = array(
 		'datasource' => 'Database/Mysql',
